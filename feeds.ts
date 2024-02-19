feb15ca47509e7102b5c7bad936ec5ff35422bcc
koki-develop <kou.pg.0131@gmail.com>
2024-02-19 20:10:11 +0900

    Add publickey

--- feeds.ts
+++ feeds.ts
@@ -1,4 +1,5 @@
 export const feeds = [
   "https://zenn.dev/feed", // Zennのトレンド
   "https://b.hatena.ne.jp/hotentry/it.rss", // はてなブックマーク - 人気エントリー - テクノロジー
+  "https://www.publickey1.jp/atom.xml", // Publickey
 ] as const;
