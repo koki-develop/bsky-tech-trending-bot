e0536ffb39cfac2480a9f572b288f9a5c9b0e58a
koki-develop <kou.pg.0131@gmail.com>
2024-03-16 18:37:58 +0900

    Revert "Add error handling to bot task execution"
    This reverts commit f744c563ea7960afb01ebb2b8a150978e9431ddf.

--- src/tasks/bot.ts
+++ src/tasks/bot.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
 import { AppBskyEmbedExternal } from "@atproto/api";
-import Errors from "aggregate-error";
 import { OgObject } from "open-graph-scraper/dist/lib/types";
 import { feeds } from "../../feeds";
 import { login, post, uploadImage } from "../lib/bsky";
@@ -21,83 +20,72 @@ import { sleep } from "../lib/util";
     "| --- | --- |",
   ];
 
-  // biome-ignore lint/suspicious/noExplicitAny:
-  const errors: any[] = [];
-
   for (const feedUrl of feeds) {
-    try {
-      const feed = await fetchRSS(feedUrl);
+    const feed = await fetchRSS(feedUrl);
 
-      feed.items.reverse(); // oldest first
-      for (const item of feed.items) {
-        await sleep(1000);
+    feed.items.reverse(); // oldest first
+    for (const item of feed.items) {
+      await sleep(1000);
 
-        if (item.link == null) {
-          logger.warn("No link", item);
-          continue;
-        }
-        logger.info("Processing", { link: item.link });
-
-        const exists = await existsItem(item.link);
-        if (exists) {
-          logger.info("Already posted", { link: item.link });
-          continue;
-        }
-
-        const ogp = await fetchOGP(item.link).catch((err) => {
-          logger.warn("Failed to fetch OGP", err);
-          return {} as OgObject;
-        });
-        const title = ogp.ogTitle ?? item.title;
-        if (title == null) {
-          logger.warn("No title", item);
-          continue;
-        }
-
-        const params: { text: string; embed?: AppBskyEmbedExternal.Main } = {
-          text: `${title}\n${item.link}`,
-        };
-
-        const embed: AppBskyEmbedExternal.Main = {
-          $type: "app.bsky.embed.external",
-          external: {
-            title,
-            description: ogp.ogDescription ?? "",
-            uri: item.link,
-          },
-        };
-
-        const imageUrl = ogp.ogImage?.at(0)?.url;
-        if (imageUrl != null) {
-          await fetchImage(imageUrl)
-            .then(async (image) => {
-              const resized = await resizeImage(image, 800);
-              const blob = await uploadImage(
-                agent,
-                new Uint8Array(resized),
-                "image/jpeg",
-              );
-              embed.external.thumb = blob;
-            })
-            .catch((err) => {
-              logger.warn("Failed to fetch image", err);
-            });
-        }
-        params.embed = embed;
-
-        await post(agent, params.text, params.embed);
-        await saveItem(item.link);
-
-        summaryLines.push(`| ${title} | ${item.link} |`);
+      if (item.link == null) {
+        logger.warn("No link", item);
+        continue;
       }
-    } catch (err) {
-      errors.push(err);
+      logger.info("Processing", { link: item.link });
+
+      const exists = await existsItem(item.link);
+      if (exists) {
+        logger.info("Already posted", { link: item.link });
+        continue;
+      }
+
+      const ogp = await fetchOGP(item.link).catch((err) => {
+        logger.warn("Failed to fetch OGP", err);
+        return {} as OgObject;
+      });
+      const title = ogp.ogTitle ?? item.title;
+      if (title == null) {
+        logger.warn("No title", item);
+        continue;
+      }
+
+      const params: { text: string; embed?: AppBskyEmbedExternal.Main } = {
+        text: `${title}\n${item.link}`,
+      };
+
+      const embed: AppBskyEmbedExternal.Main = {
+        $type: "app.bsky.embed.external",
+        external: {
+          title,
+          description: ogp.ogDescription ?? "",
+          uri: item.link,
+        },
+      };
+
+      const imageUrl = ogp.ogImage?.at(0)?.url;
+      if (imageUrl != null) {
+        await fetchImage(imageUrl)
+          .then(async (image) => {
+            const resized = await resizeImage(image, 800);
+            const blob = await uploadImage(
+              agent,
+              new Uint8Array(resized),
+              "image/jpeg",
+            );
+            embed.external.thumb = blob;
+          })
+          .catch((err) => {
+            logger.warn("Failed to fetch image", err);
+          });
+      }
+      params.embed = embed;
+
+      await post(agent, params.text, params.embed);
+      await saveItem(item.link);
+
+      summaryLines.push(`| ${title} | ${item.link} |`);
     }
   }
 
   fs.writeFileSync("summary.md", summaryLines.join("\n"));
-
-  if (errors.length > 0) {
-    throw new Errors(errors);
-  }
 })();
