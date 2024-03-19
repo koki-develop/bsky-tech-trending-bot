c07eea8492169aadb23bc582bebb4d9440e662ee
koki-develop <kou.pg.0131@gmail.com>
2024-03-19 20:39:44 +0900

    Revert "Add publickey"
    This reverts commit feb15ca47509e7102b5c7bad936ec5ff35422bcc.

--- feeds.ts
+++ feeds.ts
@@ -1,5 +1,4 @@
 export const feeds = [
   "https://zenn.dev/feed", // Zennのトレンド
   "https://b.hatena.ne.jp/hotentry/it.rss", // はてなブックマーク - 人気エントリー - テクノロジー
-  "https://www.publickey1.jp/atom.xml", // Publickey
 ] as const;
